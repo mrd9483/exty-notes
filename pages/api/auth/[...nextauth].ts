cf2e8f3e717ce6b7cc75b3b8b1a6a18bbd594c5d
Matthew Doyle <matthewrdoyle@gmail.com>
2023-02-24 19:15:01 -0500

    mongo connect

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,6 +4,7 @@ import EmailProvider from 'next-auth/providers/email';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import clientPromise from '../../../lib/mongodb';
 import User from '@/data/models/User';
+import DbConnection from '@/data/DbConnection';
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, authOptions);
 export default authHandler;
@@ -32,6 +33,8 @@ const authOptions: NextAuthOptions = {
     callbacks: {
         session: async ({ session, token }) => {
             if (session?.user) {
+                await DbConnection();
+                
                 const user = await User.findById(token.sub);
                 session.user.id = token.sub;
                 session.user.darkMode = user.darkMode ?? false;
