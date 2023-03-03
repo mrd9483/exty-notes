7e43ad62d50aa52c68851b678e856e879ae8f8f6
Matthew Doyle <matthewrdoyle@gmail.com>
2023-03-02 22:30:56 -0500

    added loading bar for notes (and probably other things)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { MantineProvider } from '@mantine/core';
 import { SessionProvider } from 'next-auth/react';
 import { ToastContainer } from 'react-toastify';
+import NextNProgress from 'nextjs-progressbar';
 
 import 'react-toastify/dist/ReactToastify.min.css';
 import '../styles/noteStyle.scss';
@@ -11,8 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <MantineProvider withGlobalStyles withNormalizeCSS>
             <SessionProvider session={pageProps.session}>
-                    <Component {...pageProps} />
-                    <ToastContainer position="top-center" theme="colored" />
+                <NextNProgress height={7} showOnShallow />
+                <Component {...pageProps} />
+                <ToastContainer position="top-center" theme="colored" />
             </SessionProvider>
         </MantineProvider>
     );
